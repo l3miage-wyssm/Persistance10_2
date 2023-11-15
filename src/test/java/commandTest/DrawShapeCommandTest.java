5f04fbd8b23cb930e568e49927f4318c05a61f9c
l3miage-wyssm <mcnw2001@gmail.com>
2023-11-15 09:55:53 +0100

    pp

--- src/test/java/commandTest/DrawShapeCommandTest.java
+++ src/test/java/commandTest/DrawShapeCommandTest.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -16,76 +15,75 @@ import edu.uga.miage.m1.polygons.gui.shapes.Circle;
 import edu.uga.miage.m1.polygons.gui.shapes.Square;
 import edu.uga.miage.m1.polygons.gui.shapes.Triangle;
 
-
 class DrawShapeCommandTest {
 
-	private Visitable circle = new Circle(5,5) ;
-	
-	private Visitable triangle = new Triangle(5,5);
-	
-	private Visitable square = new Square(5,5);
-	
+	private Visitable circle = new Circle(5, 5);
+
+	private Visitable triangle = new Triangle(5, 5);
+
+	private Visitable square = new Square(5, 5);
+
 	private List<Visitable> shapesList = new ArrayList<>();
-	
+
 	private List<Visitable> shapesListExpected = new ArrayList<>();
-	
-	private DrawShapeCommand drawCircle = new DrawShapeCommand(shapesList,circle);
-	
-	private DrawShapeCommand drawTriangle = new DrawShapeCommand(shapesList,triangle);
-	
+
+	private DrawShapeCommand drawCircle = new DrawShapeCommand(shapesList, circle);
+
+	private DrawShapeCommand drawTriangle = new DrawShapeCommand(shapesList, triangle);
+
 	private DrawShapeCommand drawSquare = new DrawShapeCommand(shapesList, square);
-	
+
 	@BeforeEach
-	void setUp(){
+	void setUp() {
 		shapesListExpected.clear();
 		shapesList.clear();
 	}
-	
+
 	@Test
 	void testExecuteCircle() {
 		drawCircle.execute();
-		
+
 		shapesListExpected.add(circle);
 		assertEquals(shapesList.get(0), shapesListExpected.get(0));
 	}
-	
+
 	@Test
-	void testExecuteTriangle(){
+	void testExecuteTriangle() {
 		drawTriangle.execute();
-		
+
 		shapesListExpected.add(triangle);
-		assertEquals(shapesList.get(0),shapesListExpected.get(0));
+		assertEquals(shapesList.get(0), shapesListExpected.get(0));
 	}
-	
+
 	@Test
-	void testExecuteSquare(){
+	void testExecuteSquare() {
 		drawSquare.execute();
-		
+
 		shapesListExpected.add(square);
-		assertEquals(shapesList.get(0),shapesListExpected.get(0));
+		assertEquals(shapesList.get(0), shapesListExpected.get(0));
 	}
-	
+
 	@Test
 	void testUndoCircle() {
 		drawCircle.execute();
 		drawCircle.undo();
-		
+
 		Assertions.assertTrue(shapesList.isEmpty());
 	}
-	
+
 	@Test
 	void testUndoTriangle() {
 		drawTriangle.execute();
 		drawTriangle.undo();
-		
+
 		Assertions.assertTrue(shapesList.isEmpty());
 	}
-	
+
 	@Test
 	void testUndoSquare() {
 		drawSquare.execute();
 		drawSquare.undo();
-		
+
 		Assertions.assertTrue(shapesList.isEmpty());
 	}
 }
--- src/test/java/shapesTest/CircleTest.java
+++ src/test/java/shapesTest/CircleTest.java
@@ -19,78 +19,79 @@ import javax.swing.JPanel;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import edu.uga.miage.m1.polygons.gui.persistence.Visitor;
 import edu.uga.miage.m1.polygons.gui.shapes.Circle;
 
-class CircleTest{
-	
+class CircleTest {
+
 	private int y = 50;
-	
+
 	private int x = 50;
-	
+
 	private String typeExpected = "circle";
-	
+
 	private Circle circle;
-	
+
 	private JPanel mPanel;
-	
+
 	private Graphics2D g2D;
-		
+
 	private Graphics2D g2DExpected;
-	
-	
+
 	@BeforeEach
 	void setUp() {
-		circle = new Circle(x,y);
-		
+		circle = new Circle(x, y);
+
 		mPanel = new JPanel();
-        mPanel.setBackground(Color.WHITE);
-        mPanel.setLayout(null);
-        mPanel.setMinimumSize(new Dimension(400, 400));
-        
-        MockitoAnnotations.initMocks(this);
-        
-        g2D = mock(Graphics2D.class);
-        
-        g2DExpected = mock(Graphics2D.class);
-        
-        when(g2D.getRenderingHint(RenderingHints.KEY_ANTIALIASING)).thenReturn(RenderingHints.VALUE_ANTIALIAS_OFF);
-        
-        when(g2DExpected.getRenderingHint(RenderingHints.KEY_ANTIALIASING)).thenReturn(RenderingHints.VALUE_ANTIALIAS_OFF);
-        
+		mPanel.setBackground(Color.WHITE);
+		mPanel.setLayout(null);
+		mPanel.setMinimumSize(new Dimension(400, 400));
+
+		// g2D = mock(Graphics2D.class);
+
+		// g2DExpected = mock(Graphics2D.class);
+
+		// when(g2D.getRenderingHint(RenderingHints.KEY_ANTIALIASING)).thenReturn(RenderingHints.VALUE_ANTIALIAS_OFF);
+
+		// when(g2DExpected.getRenderingHint(RenderingHints.KEY_ANTIALIASING))
+		// .thenReturn(RenderingHints.VALUE_ANTIALIAS_OFF);
+
 	}
-	
+
 	@Test
-	void testConstruct(){
-		assertEquals(typeExpected,circle.getType());
-		assertEquals( this.x-25,circle.getX());
-		assertEquals(this.y-25, circle.getY());
+	void testConstruct() {
+		assertEquals(typeExpected, circle.getType());
+		assertEquals(this.x - 25, circle.getX());
+		assertEquals(this.y - 25, circle.getY());
 	}
-	
+
 	@Test
 	void testDraw() {
 		circle.draw(g2D);
-		
+
 		float f = (float) x + 50;
 		g2DExpected.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		GradientPaint gradient = new GradientPaint(x, y, Color.BLUE,f, y, Color.WHITE);
-        g2DExpected.setPaint(gradient);
-        g2DExpected.fill(new Rectangle2D.Double(x, y, 50, 50));
-        BasicStroke wideStroke = new BasicStroke(2.0f);
-        g2DExpected.setColor(Color.black);
-        g2DExpected.setStroke(wideStroke);
-        g2DExpected.draw(new Rectangle2D.Double(x, y, 50, 50));
-		
-        assertEquals(g2D.getRenderingHint(RenderingHints.KEY_ANTIALIASING),g2DExpected.getRenderingHint(RenderingHints.KEY_ANTIALIASING));
+		GradientPaint gradient = new GradientPaint(x, y, Color.BLUE, f, y, Color.WHITE);
+		g2DExpected.setPaint(gradient);
+		g2DExpected.fill(new Rectangle2D.Double(x, y, 50, 50));
+		BasicStroke wideStroke = new BasicStroke(2.0f);
+		g2DExpected.setColor(Color.black);
+		g2DExpected.setStroke(wideStroke);
+		g2DExpected.draw(new Rectangle2D.Double(x, y, 50, 50));
+
+		assertEquals(g2D.getRenderingHint(RenderingHints.KEY_ANTIALIASING),
+				g2DExpected.getRenderingHint(RenderingHints.KEY_ANTIALIASING));
 	}
-	
+
 	@Test
 	void testAccept() {
 		Visitor visitor = mock(Visitor.class);
-		
+
 		circle.accept(visitor);
-		
+
 		verify(visitor, times(1)).visit(circle);
 	}
 }
